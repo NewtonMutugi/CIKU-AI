68bca1792503f6cb4a868803e3cacf7bdd2b0e20
NewtonMutugi <newtonmut197@gmail.com>
2023-12-17 15:25:34 +0000

    Added server

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :eliza_chat_v3, ElizaChatV3Web.Endpoint,
+  server: true,
   url:  [host: System.get_env("EXTERNAL_HOSTNAME") || "35.212.167.138", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 

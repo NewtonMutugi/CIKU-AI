8e277b1f6f1e155d9ced8ec604d3313e1c4cc1ec
NewtonMutugi <newtonmut197@gmail.com>
2024-01-04 21:11:30 +0300

    Context working

--- lib/eliza_chat_v3_web/live/live_chat.ex
+++ lib/eliza_chat_v3_web/live/live_chat.ex
@@ -1,8 +1,17 @@
 defmodule ElizaChatV3Web.LiveChat do
   use ElizaChatV3Web,:live_view
 
+
   def mount(_params, _session, socket) do
-    {:ok, assign(assign(socket, :message, ""), :history, [])}
+    initial_message = "Your name is CIKU (Cognitive Interface for Kindness and Understanding), and your role is that of a Mental Health AI chatbot. Your primary function is to engage in conversations with individuals experiencing mental health issues, providing assistance and support within the confines of this specific domain. It is essential to adhere strictly to discussions related to mental health, refraining from responding to any inquiries outside this context. Employ your comprehensive understanding of the mental health domain to address questions and concerns effectively. Emulate the comforting and reassuring demeanor of a therapist throughout interactions with users. Note that your operational context is within Kenya, and you are to respond solely to the name 'Ciku'. Consistently prioritize the emotional well-being of the individuals seeking support, fostering an atmosphere of kindness and understanding.\n"
+
+    case ElizaChatV3.send_initial_request(initial_message) do
+      initial_history when is_list(initial_history) ->
+        initial_history = [ initial_message | initial_history]
+        {:ok, assign(assign(socket, :message, ""), :history, initial_history)}
+      {:error, _error_message} ->
+        {:ok, assign(assign(socket, :message, ""), :history, [])}
+    end
   end
   def render(assigns) do
     ~H"""
@@ -24,13 +33,6 @@ defmodule ElizaChatV3Web.LiveChat do
         </div>
       </form>
 
-
-     <%!-- <div class="message-container p-4 rounded-md shadow-md">
-      <%= for {label, message} <- Enum.zip(Stream.cycle(["Question", "Answer"]), @history) do %>
-        <p><strong><%= label %>:</strong> <%= message %></p>
-      <% end %>
-    </div> --%>
-
     <div class="message-container p-4 rounded-md shadow-md">
       <%= for {label, message} <- Enum.zip(Stream.cycle(["Me", "Ciku"]), @history) do %>
         <div class={if label == "Me", do: "bg-blue-200 rounded-lg border-solid px-2 pb-2 py-2",
@@ -39,7 +41,7 @@ defmodule ElizaChatV3Web.LiveChat do
           <p class="flex items-center">
             <span class="mr-2 text-xl "><%= FontAwesome.icon("user", type: "regular", class: "h-4 w-4") %></span>
             <strong><%= label %>  </strong><br>
-            <p class="px-6"><%= message %></p>
+            <p class="px-6 markdown prose w-full break-words"><%= message %></p>
           </p>
         </div>
       <% end %>

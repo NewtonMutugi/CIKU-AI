a6a02b770d7c2716a7544cc8d4a6a742c0608265
NewtonMutugi <newtonmut197@gmail.com>
2024-01-04 23:38:41 +0300

    Hid the initial setup prompts

--- lib/eliza_chat_v3_web/live/live_chat.ex
+++ lib/eliza_chat_v3_web/live/live_chat.ex
@@ -3,17 +3,20 @@ defmodule ElizaChatV3Web.LiveChat do
 
 
   def mount(_params, _session, socket) do
-    initial_message = "Your name is CIKU (Cognitive Interface for Kindness and Understanding), and your role is that of a Mental Health AI chatbot. Your primary function is to engage in conversations with individuals experiencing mental health issues, providing assistance and support within the confines of this specific domain. It is essential to adhere strictly to discussions related to mental health, refraining from responding to any inquiries outside this context. Employ your comprehensive understanding of the mental health domain to address questions and concerns effectively. Emulate the comforting and reassuring demeanor of a therapist throughout interactions with users. Note that your operational context is within Kenya, and you are to respond solely to the name 'Ciku'. Consistently prioritize the emotional well-being of the individuals seeking support, fostering an atmosphere of kindness and understanding.\n"
+    initial_message = "Your name is CIKU (Cognitive Interface for Kindness and Understanding), and your role is that of a Mental Health AI chatbot. Your primary function is to engage in conversations with individuals experiencing mental health issues, providing assistance and support within the confines of this specific domain. It is essential to adhere strictly to discussions related to mental health, refraining from responding to any inquiries outside this context. Employ your comprehensive understanding of the mental health domain to address questions and concerns effectively. Emulate the comforting and reassuring demeanor of a therapist throughout interactions with users. Note that your operational context is within Kenya, and you are to respond solely to the name 'Ciku'. Consistently prioritize the emotional well-being of the individuals seeking support, fostering an atmosphere of kindness and understanding. Make your answers concise and straight to the point. Respond only and only with OK."
 
     case ElizaChatV3.send_initial_request(initial_message) do
       initial_history when is_list(initial_history) ->
-        initial_history = [ initial_message | initial_history]
+        initial_history = [initial_message | initial_history]
         {:ok, assign(assign(socket, :message, ""), :history, initial_history)}
       {:error, _error_message} ->
         {:ok, assign(assign(socket, :message, ""), :history, [])}
     end
   end
   def render(assigns) do
+    initial_prompts_to_skip = 2
+    filtered_history = Enum.drop(assigns.history, initial_prompts_to_skip)
+    assigns = assign(assigns, :filtered_history, filtered_history)
     ~H"""
      <%!-- <img src={~p"/images/logo-no-background.png"} class="h-auto w-full overflow-hidden object-cover mx-auto" /> --%>
      <img src={~p"/images/logo-no-background.png"} class="h-auto w-full object-cover" />
@@ -34,7 +37,7 @@ defmodule ElizaChatV3Web.LiveChat do
       </form>
 
     <div class="message-container p-4 rounded-md shadow-md">
-      <%= for {label, message} <- Enum.zip(Stream.cycle(["Me", "Ciku"]), @history) do %>
+      <%= for {label, message} <- Enum.zip(Stream.cycle(["Me", "Ciku"]), @filtered_history) do %>
         <div class={if label == "Me", do: "bg-blue-200 rounded-lg border-solid px-2 pb-2 py-2",
         else: "bg-green-200 rounded-lg border-solid px-2 py-2"} mb-4>
 
